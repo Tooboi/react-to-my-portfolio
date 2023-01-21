4c6a537911ac3a6dc39c6f8bafe4a6a1e2c86127
Tooboi <japica123@gmail.com>
2023-01-21 05:01:23 -0500

    Portfolio section

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import background from '../../images/JPjumble2k2.png';
+import Project from '../Project'
+const projects = [
+    {
+        id: '',
+        image: '',
+        title: '',
+        link: '',
+        repo: ''
+    }
+]
 export default function Portfolio() {
     return (
         <div className='portfolioPage pt-5' style={{ backgroundImage: `url(${background})` }}>
