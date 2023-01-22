2651af2df51bbc5174a97c39ac9ecab680e8838b
Tooboi <japica123@gmail.com>
2023-01-21 19:52:15 -0500

    Added projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,42 +1,46 @@
 import React from 'react';
 import background from '../../images/JPjumble2k2.png';
-import UIstyle from '../../images/UIstyleGuide.gif';
+import UIstyle from '../../images/UIstyleGuide.png';
+import SoundByte from '../../images/soundByte.png';
+import AfterAfters from '../../images/afterAfters.png';
+import GammaUI from '../../images/gammaUI.png';
+import MVCtb from '../../images/techBlog.png';
 import Project from '../Project'
 const projects = [
     {
         id: 1,
         image: UIstyle,
-        title: 'First Project',
-        link: '',
-        repo: ''
+        title: 'UI Style Guide',
+        link: 'https://tooboi.github.io/Style-Guide/',
+        repo: 'https://github.com/Tooboi/Style-Guide'
     },
     {
         id: 2,
-        image: '',
-        title: 'Second Project',
-        link: '',
-        repo: ''
+        image: SoundByte,
+        title: 'Sound Byte',
+        link: 'https://soundbyte.herokuapp.com/',
+        repo: 'https://github.com/JoshuaCarter99/bandshare'
     },
     {
         id: 3,
-        image: '',
-        title: 'Third Project',
-        link: '',
-        repo: ''
+        image: AfterAfters,
+        title: 'After Afters',
+        link: 'https://tooboi.github.io/After-Afters/',
+        repo: 'https://github.com/Tooboi/After-Afters'
     },
     {
         id: 4,
-        image: '',
-        title: 'Fourth Project',
-        link: '',
-        repo: ''
+        image: GammaUI,
+        title: 'Gamma UI',
+        link: 'https://tooboi.github.io/Gamma-UI/',
+        repo: 'https://github.com/Tooboi/Gamma-UI'
     },
     {
         id: 5,
-        image: '',
-        title: 'Fifth Project',
-        link: '',
-        repo: ''
+        image: MVCtb,
+        title: 'MVC Tech Blog',
+        link: 'https://tech-blog-2023.herokuapp.com/',
+        repo: 'https://github.com/Tooboi/MVC-Tech-Blog'
     },
     {
         id: 6,
