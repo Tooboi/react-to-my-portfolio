ff4335c660f08caf158b7aff055f473e1e21058a
Tooboi <japica123@gmail.com>
2023-01-22 03:05:50 -0500

    Project Cards Complete

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,13 +5,13 @@ export default function About() {
     <div className="aboutPage py-5 px-3" style={{ backgroundImage: `url(${background})` }}>
       <div className="container  glass-2 flex-column my-4 force-v-center py-6 max-width-960">
         <div className="row justify-content-center col-6">
-         <h1 className='glass-text mb-4 px-2 text-align-center'>Hey, I'm Josh</h1>  
+         <h1 className='glass-text mb-4 px-2 text-align-center select-none'>Hey, I'm Josh</h1>  
         </div>
         <div className="row justify-content-center col-6">
-         <h2 className='glass-text mb-0 px-2 text-align-center'>I'm a Full Stack Web Developer and 3D Artist</h2>  
+         <h2 className='glass-text mb-0 px-2 text-align-center select-none'>I'm a Full Stack Web Developer and 3D Artist</h2>  
         </div>
         <div className="row justify-content-center col-6">
-         <h2 className='glass-text mb-0 px-2 text-align-center'>Based in Philadelphia, PA</h2>  
+         <h2 className='glass-text mb-0 px-2 text-align-center select-none'>Based in Philadelphia, PA</h2>  
         </div>
       </div>
     </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -14,7 +14,7 @@ const projects = [
         title: 'UI Style Guide',
         link: 'https://tooboi.github.io/Style-Guide/',
         repo: 'https://github.com/Tooboi/Style-Guide',
-        desc: ''
+        desc: 'Frontend UI style template for choosing colors, font, or random color'
     },
     {
         id: 2,
@@ -22,7 +22,7 @@ const projects = [
         title: 'Sound Byte',
         link: 'https://soundbyte.herokuapp.com/',
         repo: 'https://github.com/JoshuaCarter99/bandshare',
-        desc: ''
+        desc: 'Full stack music sharing app utilizing MySQL, Node, Express, Handlebars, and Cloudinary'
     },
     {
         id: 3,
@@ -30,7 +30,7 @@ const projects = [
         title: 'After Afters',
         link: 'https://tooboi.github.io/After-Afters/',
         repo: 'https://github.com/Tooboi/After-Afters',
-        desc: ''
+        desc: 'Frontend app that allows the user to find sports or music events in Philly, then find food within a 15 minute walk of the venue'
     },
     {
         id: 4,
@@ -38,7 +38,7 @@ const projects = [
         title: 'Gamma UI',
         link: 'https://tooboi.github.io/Gamma-UI/',
         repo: 'https://github.com/Tooboi/Gamma-UI',
-        desc: ''
+        desc: 'My own css framework for getting apps off the ground quickly with a flat UI'
     },
     {
         id: 5,
@@ -46,7 +46,7 @@ const projects = [
         title: 'MVC Tech Blog',
         link: 'https://tech-blog-2023.herokuapp.com/',
         repo: 'https://github.com/Tooboi/MVC-Tech-Blog',
-        desc: ''
+        desc: 'Full stack MVC tech blog utilizing MySQL, Node, Express, and Handlebars'
     },
     {
         id: 6,
@@ -54,12 +54,13 @@ const projects = [
         title: 'Weather Dashboard',
         link: 'https://tooboi.github.io/Weather-Dashboard/',
         repo: 'https://github.com/Tooboi/Weather-Dashboard',
-        desc: ''
+        desc: 'Frontend weather app utilizing restful API'
     },
 ]
 export default function Portfolio() {
     return (
-        <div className='portfolioPage pt-5 container' style={{ backgroundImage: `url(${background})` }}>
+        <div className='portfolioPage pt-5 container flex-column align-items-center' style={{ backgroundImage: `url(${background})` }}>
+            <h1 className='mt-4 inline-flex justify-content-center glass-2 px-3 select-none'>Recent Projects</h1>
             <Project projects={projects} image={projects.image} title={projects.title} link={projects.link} repo={projects.repo} desc={projects.desc} />
         </div>
     );
