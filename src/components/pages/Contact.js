0d7d0b6df2fcd88edaef8197fd5862b0c4b3df05
Tooboi <japica123@gmail.com>
2023-01-22 19:15:13 -0500

    Contact Card

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import background from '../../images/JPjumble2k3.png';
 export default function Contact() {
-    return (
-        <div className='contactPage pt-5' style={{ backgroundImage: `url(${background})` }}>
-            <h1 className='mt-4 inline-flex justify-content-center glass-2 px-3 select-none'>Contact</h1>
+  return (
+    <div className="contactPage pt-5 container flex-column align-items-center" style={{ backgroundImage: `url(${background})` }}>
+      <h1 className="m-5 inline-flex justify-content-center glass-2 px-3 select-none">Contact Me</h1>
+      <div className="glass-2 p-4 row inline-flex justify-content-center">
+        <div class="justify-content-center">
+          <div class="p-2 mx-2 justify-content-space-evenly">
+           <form>
+            <div className='row'>
+                <label for='name' className='mb-0'>Name</label>
+                <input id='name' placeholder='Elfo'></input>
+            </div>
+                
+           </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
